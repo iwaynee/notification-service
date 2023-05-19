d980fac34503624eb3347b05c8925cad3d9af01b
iwaynee <ivan.heinzer@hotmail.ch>
2023-05-19 14:59:36 +0200

    update

--- src/env.ts
+++ src/env.ts
@@ -12,7 +12,7 @@ export const env = {
         environment: process.env.NODE_ENV || "production"
     },
     db: {
-        url: "mongodb://" + process.env.DB_HOST || "",
+        url: process.env.DB_HOST || "",
         database: process.env.DB_DATABASE || "",
         username: process.env.DB_USER || "",
         password: process.env.DB_PASSWORD || ""
--- src/util/databaseConnection.ts
+++ src/util/databaseConnection.ts
@@ -2,13 +2,13 @@ import {DataSource} from "typeorm";
 import {env} from "../env";
 import {logger} from "./logger";
 
+console.log(`mongodb://${env.db.username}:${env.db.password}@${env.db.url}/?authMechanism=SCRAM-SHA-256`)
+
 const dataSource = new DataSource({
     type: "mongodb",
-    url: env.db.url,
-    username: env.db.username,
-    password: env.db.password,
-    database: env.db.database,
     authSource: 'admin',
+    database: 'nosleep_hub',
+    url: `mongodb://${env.db.username}:${env.db.password}@${env.db.url}/`,
     entities: [
         env.app.environment === "production" ? "build/entities/**/*.js" : "src/entities/**/*.ts"
     ],
