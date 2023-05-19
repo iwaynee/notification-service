a294406c0d00f1592424bb27419010b8ff0f45fa
iwaynee <ivan.heinzer@hotmail.ch>
2023-05-19 13:20:20 +0200

    update

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` })
 
+console.log(process.env.TEST)
+
 export const env = {
     app: {
         name: process.env.APP_NAME ||"",
